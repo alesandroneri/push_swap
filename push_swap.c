e97afb2fc1e11225c1a64d1b65dba25ce5644f40
aneri-da <alesssandro.soares1604@gmail.com>
2025-01-28 14:35:51 -0300

    finalizacao definitiva da push_swap

--- push_swap.c
+++ push_swap.c
@@ -6,19 +6,19 @@
 /*   By: aneri-da <aneri-da@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 10:50:33 by aneri-da          #+#    #+#             */
-/*   Updated: 2025/01/22 14:36:20 by aneri-da         ###   ########.fr       */
+/*   Updated: 2025/01/28 14:31:19 by aneri-da         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/push_swap.h"
 
-void	valid_arguments(int ac, char **av, t_stack **stack_a)
+int	valid_arguments(int ac, char **av, t_stack **stack_a)
 {
 	char	**split_args;
 	int		i;
 
 	if (ac == 1 || (ac == 2 && !av[1][0]))
-		return ;
+		return (0);
 	i = 0;
 	while (++i < ac)
 	{
@@ -26,18 +26,19 @@ void	valid_arguments(int ac, char **av, t_stack **stack_a)
 		if (!split_args)
 		{
 			ft_putendl_fd("Error", 2);
-			return ;
+			return (0);
 		}
 		if (!ft_check_arguments(ft_count_split(split_args), split_args))
 		{
 			ft_free_split(split_args);
 			ft_free_stack(stack_a);
 			ft_putendl_fd("Error", 2);
-			return ;
+			return (0);
 		}
 		init_stack_a(stack_a, split_args);
 		ft_free_split(split_args);
 	}
+	return (1);
 }
 
 void	sorting(t_stack **stack_a, t_stack **stack_b)
@@ -84,7 +85,8 @@ int	main(int ac, char **av)
 
 	a = NULL;
 	b = NULL;
-	valid_arguments(ac, av, &a);
+	if (valid_arguments(ac, av, &a) == 0)
+		return (0);
 	sort_stack(&a, &b);
 	ft_free_stack(&a);
 	ft_free_stack(&b);
